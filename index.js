7d93776cebbf05c7f20520187954c912f59f839a
Francisco Valdes-Jasso <koesavant@gmail.com>
2015-02-02 11:43:12 -0600

    Some bugs

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.get('/', function(req, res){
 });
 io.on('connection', function(socket){
   console.log('a user connected');
-});
-io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
